44cf9199faa6e2403c05e8bfd80d069e1052d3d5
ShreyasKapoor22 <89712579+ShreyasKapoor22@users.noreply.github.com>
2023-04-04 18:17:51 -0600

    Add files to src folder

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+// Import the Navbar, Nav and Container components from Bootstrap for a nice layout
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+
+function App() {
+  return (
+    <>
+      <Navbar bg="dark" expand="lg" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">Know Your City</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          {/* <Nav className="me-auto">
+            <Nav.Link href="/posts/new">New</Nav.Link>
+          </Nav> */}
+        </Container>
+      </Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
